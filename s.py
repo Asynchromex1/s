44dc17d11f6d51d949e72ecffe8d9203e594c75a
Asynchromex1 <163177247+Asynchromex1@users.noreply.github.com>
2024-03-17 12:10:05 +0300

    Update s.py

--- s.py
+++ s.py
@@ -1,34 +1,34 @@
 import socket
 
-# Sunucunun IP adresi ve port numarası
-HOST = '0.0.0.0'  # Tüm ağ arayüzlerini dinle
+ 
+HOST = '0.0.0.0'  # 
 PORT = 8080
-
-# IPv4, TCP soketi oluştur
+print("Bağlantı başarılı")
+ 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Soketi belirtilen HOST ve PORT'a bağla
+ 
     s.bind((HOST, PORT))
-    # Bağlantıları dinlemeye başla, en fazla 5 bağlantıyı kabul et
+    
     s.listen(5)
     print(f"Sunucu {HOST}:{PORT} üzerinde dinlemede...")
 
-    # Sonsuz döngüde bağlantıları kabul et
+     
     while True:
-        # İstemci bağlantısını kabul et
+      
         conn, addr = s.accept()
         print(f"Bağlantı alındı from {addr}")
 
-        # İstemciden gelen veriyi al
+        
         data = conn.recv(1024)
         if not data:
-            break  # Veri yoksa döngüden çık
+            break  
 
-        # Gelen veriyi ekrana yazdır
+     
         print(f"Gelen veri: {data.decode()}")
 
-        # İstemciye cevap gönder
+ 
         response = "HTTP/1.1 200 OK\n\nHello, client!"
         conn.sendall(response.encode())
 
-        # Bağlantıyı kapat
+     
         conn.close()
